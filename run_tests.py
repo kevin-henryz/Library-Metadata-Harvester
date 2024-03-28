198d1048bc18ed5d721b4a9c653a97e6699d4db5
kevin-henryz <kevinhenry2023@gmail.com>
2024-03-27 22:20:37 -0300

    Updated pytest with plugin

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import subprocess
 
-pytest_command = ["pytest"]  # Add other pytest options here as needed
+pytest_command = ["pytest", "-n", "auto"]  
 
 # Run the command
 subprocess.run(pytest_command)
